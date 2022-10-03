5c334028cadd28f9e9f1cce5003aac515816d87b
Houssem Dalla <60631406+houssem0409@users.noreply.github.com>
2022-10-04 00:31:49 +0100

    dev-change

--- Models/user.js
+++ Models/user.js
@@ -1,30 +1,32 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-    {
+  {
     name: {
-        type: String,
-        trim: true,
-        required: true,
-        maxlength:32,
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
     },
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        maxlength:32,
-        unique: true
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      unique: true,
     },
     password: {
-        type: String,
-        trim: true,
-        required: true,
-    }
-}, 
-{timestamps:true}
+      type: String,
+      trim: true,
+      required: true,
+    },
+    age: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
-
-
-module.exports = mongoose.model("User" ,  userSchema);
+module.exports = mongoose.model("User", userSchema);
